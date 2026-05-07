489b5dd52f946a14cc61fcea136736a396589616
jyeoniop <wldud128iop@gmail.com>
2026-05-07 17:25:18 +0900

    [level 2] Title: 시소 짝꿍, Time: 30.40 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -8,31 +8,27 @@ class Solution {
         for(int weight : weights){
             hmap.put(weight, hmap.getOrDefault(weight,0)+1);
         }
-        Integer[] uniqueWeights = hmap.keySet().toArray(new Integer[0]);
+        Integer[] uniqueWeights = hmap.keySet().toArray(Integer[]::new);
         Arrays.sort(uniqueWeights);
         
         for (int w : uniqueWeights) {
             long count = (long) hmap.get(w);
             
-            // Case 1: 같은 몸무게끼리 짝꿍 (nC2 조합)
             if (count > 1) {
                 answer += (count * (count - 1)) / 2;
             }
 
-            // Case 2: 다른 몸무게와 짝꿍 (비율 계산)
-            // 시소 거리가 2, 3, 4이므로 가능한 비율은 3/2, 4/2(2/1), 4/3 입니다.
-            // w보다 큰 몸무게들 중에서 찾아야 중복 카운트가 안 됩니다.
-            int[] targets = {w * 3, w * 4, w * 4};
-            int[] divisors = {2, 2, 3};
-
-            for (int i = 0; i < 3; i++) {
-                if (targets[i] % divisors[i] == 0) {
-                    int targetWeight = targets[i] / divisors[i];
-                    if (hmap.containsKey(targetWeight)) {
-                        answer += count * (long) hmap.get(targetWeight);
+            int[] target = {w*4, w*4, w*3};
+            int[] divisor = {3, 2, 2};
+            
+            for(int i=0;i<3;i++){
+                if(target[i]%divisor[i]==0){
+                    if(hmap.containsKey(target[i]/divisor[i])){
+                        answer += count*hmap.get(target[i]/divisor[i]);
                     }
                 }
             }
+            
         }
         return answer;
     }
