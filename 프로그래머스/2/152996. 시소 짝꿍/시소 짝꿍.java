6bf1b705bec5e40de2ce4ced295079ff2f7499ba
jyeoniop <wldud128iop@gmail.com>
2026-05-11 17:12:58 +0900

    [level 2] Title: 시소 짝꿍, Time: 27.83 ms, Memory: 68.9 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -11,25 +11,20 @@ class Solution {
         Integer[] uniqueWeights = hmap.keySet().toArray(Integer[]::new);
         Arrays.sort(uniqueWeights);
         
-        for (int w : uniqueWeights) {
-            long count = (long) hmap.get(w);
+        for(int w:uniqueWeights){
+            long count = (long)hmap.get(w);
+            int[] divisor = {3,2,2};
+            int[] multi = {4,4,3};
             
-            if (count > 1) {
-                answer += (count * (count - 1)) / 2;
-            }
-
-            int[] target = {w*4, w*4, w*3};
-            int[] divisor = {3, 2, 2};
+            answer += (count*(count-1)/2);
             
             for(int i=0;i<3;i++){
-                if(target[i]%divisor[i]==0){
-                    if(hmap.containsKey(target[i]/divisor[i])){
-                        answer += count*hmap.get(target[i]/divisor[i]);
-                    }
+                if(w*multi[i]%divisor[i]==0&&hmap.containsKey((w*multi[i]/divisor[i]))){
+                    answer += count*(hmap.get(w*multi[i]/divisor[i]));
                 }
             }
-            
         }
+        
         return answer;
     }
 }
