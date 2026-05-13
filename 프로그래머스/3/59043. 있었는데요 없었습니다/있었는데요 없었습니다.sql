05f46da84404960fc07313478d27b02aeb63f7ce
jyeoniop <wldud128iop@gmail.com>
2026-05-13 17:20:13 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,5 +1,19 @@
--- 코드를 입력하세요
-SELECT i.animal_id, i.name
-from ANIMAL_INS as i join ANIMAL_OUTS  as o on i.animal_id = o.animal_id
-where i.DATETIME>o.datetime
+
+
+
+select i.animal_id, i.name
+from ANIMAL_INS as i join ANIMAL_OUTS as o on i.animal_id=o.animal_id
+where i.datetime>o.datetime
 order by i.datetime asc
+
+
+
+
+
+
+
+
+
+
+
+
