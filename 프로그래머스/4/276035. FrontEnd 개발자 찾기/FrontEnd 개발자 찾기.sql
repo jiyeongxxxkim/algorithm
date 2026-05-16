3f26461a470ee4936e6c8e4b406f8d0903f4c167
jyeoniop <wldud128iop@gmail.com>
2026-05-16 19:03:44 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -1,4 +1,31 @@
-select DISTINCT d.id, d.email, d.first_name, d.last_name
-from DEVELOPERS as d join SKILLCODES  as s on (d.skill_code&s.code)>0
-where s.CATEGORY='Front End'
-order by d.id asc
+# select distinct d.id, d.email, d.first_name, d.last_name
+# from skillcodes as s join DEVELOPERS as d on (s.code&d.skill_code)>0
+# where s.category='Front End'
+# order by d.id asc;
+
+
+
+# select distinct d.id, d.email, d.first_name, d.last_name
+# from developers as d join skillcodes as s on (d.skill_code&s.code)>0
+# where s.category = 'Front End'
+# order by d.id asc;
+
+select id, email, first_name, last_name
+from DEVELOPERS as d 
+where (skill_code&(select sum(code) from skillcodes where category="Front End"))>0
+order by id asc
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
