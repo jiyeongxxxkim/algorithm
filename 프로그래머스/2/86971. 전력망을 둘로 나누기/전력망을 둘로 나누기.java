877a14da08e216c7db9214db971b8bc6f981af64
jyeoniop <wldud128iop@gmail.com>
2026-05-18 19:42:53 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.11 ms, Memory: 63.1 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,16 +1,15 @@
 import java.util.*;
 class Solution {
-    static ArrayList<ArrayList<Integer>> graph;
-    static boolean[] visited;
+    ArrayList<ArrayList<Integer>> graph;
+    boolean[] visited;
     public int bfs(int start, int skip){
         Queue<Integer> q = new LinkedList<>();
-        
         q.add(start);
+        int count  = 1;
         visited[start] = true;
-        int count = 1;
         while(!q.isEmpty()){
             int cur = q.poll();
-            for(int i : graph.get(cur)){
+            for(int i:graph.get(cur)){
                 if(!visited[i]&&i!=skip){
                     visited[i] = true;
                     q.add(i);
@@ -19,36 +18,28 @@ class Solution {
             }
         }
         return count;
-        
     }
     public int solution(int n, int[][] wires) {
         int answer = -1;
         graph = new ArrayList<>();
         for(int i=0;i<=n;i++){
-            graph.add(new ArrayList<Integer>());
+            graph.add(new ArrayList<>());
         }
-        
         for(int i=0;i<n-1;i++){
             graph.get(wires[i][0]).add(wires[i][1]);
             graph.get(wires[i][1]).add(wires[i][0]);
         }
-               
+        
         int min = Integer.MAX_VALUE;
         int minus = 0;
         for(int i=0;i<n-1;i++){
             visited = new boolean[n+1];
-            // System.out.println("bfschk : "+bfs(3,4));
-            
-            int a = wires[i][0];
-            int b = wires[i][1];
-            minus = Math.abs(n-(bfs(a,b)*2));
-            min = Math.min(min, minus);  
-       
+            int A = bfs(wires[i][0], wires[i][1]);
+            int B = n - A;
+            minus = Math.abs(A - B);
+            min = Math.min(min, minus);
         }
-        answer = min;
-        return answer;
         
-        
-    }  
-    
+        return min;
+    }
 }
