3cebd39afbc6eecde7862e40e6ff40f8be36c574
jyeoniop <wldud128iop@gmail.com>
2026-05-26 18:52:16 +0900

    [level 2] Title: 괄호 회전하기, Time: 11.45 ms, Memory: 63.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Solution {
+    public boolean istrue(String str){
+        ArrayDeque<Character> stk = new ArrayDeque<>();
+        for(int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+            if(ch=='('||ch=='{'||ch=='['){
+                stk.push(ch);
+            }else{
+                if(stk.isEmpty())return false;
+                char popch = stk.pop();
+                if(ch==')'&&popch!='(')return false;
+                if(ch=='}'&&popch!='{')return false;
+                if(ch==']'&&popch!='[')return false;
+            }
+        }
+        return stk.isEmpty();
+    }
+    public int solution(String s) {
+        int answer = 0;
+        int len = s.length();
+        if(s.length()%2==1)return 0;
+        
+        String doubles = s+s;
+        
+        for(int i=0;i<len;i++){
+            String chkstr = doubles.substring(i, i+len);
+            if(istrue(chkstr))answer++;
+        }
+        
+        return answer;
+    }
+}
