c104841372b7e87f41c4ea1d29f3426b4d2546c1
jiyeongxxxkim <wldud128iop@gmail.com>
2026-06-03 15:31:45 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 48.68 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -1,17 +1,32 @@
 class Solution {
+    int getidx(String n){
+        for(int i=n.length()-1;i>=0;i--){
+            if(n.charAt(i)=='0')return n.length()-i-1;
+        }
+        return -1;
+    }
+    long change(String n, int idx){
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<n.length();i++){
+            if(i==(n.length()-idx-1))sb.append("1");
+            else if(i==(n.length()-idx))sb.append("0");
+            else sb.append(n.charAt(i));
+        }
+        return Long.parseLong(sb.toString(), 2);
+    }
     public long[] solution(long[] numbers) {
         long[] answer = new long[numbers.length];
-        
         for(int i=0;i<numbers.length;i++){
-            long number = numbers[i];
-            
-            if(number%2==0)answer[i] = number+1;
+            long num = numbers[i];
+            String n = "0"+Long.toBinaryString(num);
+            // System.out.println(n+" : "+getidx(n));
+            if(getidx(n)==0)answer[i] = (long)(numbers[i]+1);
             else{
-                long lastZero = ~number & (number + 1); 
-                answer[i] = (number | lastZero) & ~(lastZero >> 1);
+                answer[i] = change(n, getidx(n));
+                // System.out.println(change(n, getidx(n)));
             }
         }
-        
+
         return answer;
     }
 }
