abe9b1d165146446ee2cd82d203724cdab24c25e
jiyeongxxxkim <wldud128iop@gmail.com>
2026-06-06 15:20:07 +0900

    [level 2] Title: 시소 짝꿍, Time: 30.92 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -2,29 +2,24 @@ import java.util.*;
 class Solution {
     public long solution(int[] weights) {
         long answer = 0;
-        Map<Integer, Integer> hmap = new HashMap<>();
-        Arrays.sort(weights);
-        
-        for(int weight : weights){
-            hmap.put(weight, hmap.getOrDefault(weight,0)+1);
+        TreeMap<Integer, Long> tmap = new TreeMap<>();
+        for(int we:weights){
+            tmap.put(we, tmap.getOrDefault(we, 0L)+(long)1);
         }
-        Integer[] uniqueWeights = hmap.keySet().toArray(Integer[]::new);
-        Arrays.sort(uniqueWeights);
+        int[] maxarr = {1,2,3};
+        int[] minarr = {2,3,4};
         
-        for(int w:uniqueWeights){
-            long count = (long)hmap.get(w);
-            int[] divisor = {3,2,2};
-            int[] multi = {4,4,3};
-            
-            answer += (count*(count-1)/2);
-            
+        for(int key:tmap.keySet()){
+            if(tmap.get(key)>1){
+                answer += (tmap.get(key)*(tmap.get(key)-1)/2);
+            }
             for(int i=0;i<3;i++){
-                if(w*multi[i]%divisor[i]==0&&hmap.containsKey((w*multi[i]/divisor[i]))){
-                    answer += count*(hmap.get(w*multi[i]/divisor[i]));
+                if((minarr[i]*key)%maxarr[i]==0&&tmap.containsKey((minarr[i]*key)/maxarr[i])){
+                    System.out.println(tmap.get(key)*tmap.get(minarr[i]*key/maxarr[i]));
+                    answer += (long)(tmap.get(key)*tmap.get(minarr[i]*key/maxarr[i]));
                 }
             }
         }
-        
         return answer;
     }
 }
