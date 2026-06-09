1e8b240e8d72aff9bf140fac47df03de2e54f54f
jiyeongxxxkim <wldud128iop@gmail.com>
2026-06-09 17:53:02 +0900

    [level 3] Title: 순위, Time: 5.75 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -11,7 +11,6 @@ class Solution {
             score[lose][win] = -1;
             
         }
-        
         for(int k=1;k<=n;k++){
             for(int i=1;i<=n;i++){
                 for(int j=1;j<=n;j++){
