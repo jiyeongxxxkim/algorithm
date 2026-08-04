ca00d9375cf25e5dd0b436fe94fe5754c2f97dd4
jiyeongxxxkim <wldud128iop@gmail.com>
2026-08-04 22:23:54 +0900

    [level 2] Title: 의상, Time: 0.13 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,16 +2,17 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 0;
-        int len = clothes.length;
+        
         HashMap<String, Integer> clo = new HashMap<>();
-        for(int i=0;i<len;i++){
-            clo.put(clothes[i][1], clo.getOrDefault(clothes[i][1],0)+1);
-        }
-        int total =1;
-        for(String s : clo.keySet()){
-            total  *= (clo.get(s)+1);
+        for(String[] str :clothes){
+            clo.put(str[1], clo.getOrDefault(str[1], 0)+1);
+        } 
+        int total = 1;
+        for(String k:clo.keySet()){
+            total*=(clo.get(k)+1);
         }
         answer = total-1;
+        
         return answer;
     }
 }
