b45ea1674cd783b525aaa7e2987eb64c5e0e5fda
jiyeongxxxkim <wldud128iop@gmail.com>
2026-08-05 19:34:34 +0900

    [level 3] Title: 선입 선출 스케줄링, Time: 2.93 ms, Memory: 58.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/12920. 선입 선출 스케줄링/선입 선출 스케줄링.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int solution(int n, int[] cores) {
+        if(cores.length>=n)return n;
+        
+        int maxcore = 0;
+        for(int c:cores)maxcore = Math.max(maxcore, c);
+        
+        long left = 0;
+        long right = (n/cores.length)*maxcore;
+        long time = right;
+        
+        while(left<=right){
+            long mid = (left+right)/2;
+            long work = cores.length;
+            for(int c:cores){
+                work += (mid/c);
+            }
+            if(work>=n){
+                time = mid;
+                right = mid-1;
+            }else{
+                left = mid+1;
+            }
+        }
+        long work = cores.length;
+        for(int c:cores){
+            work += (time-1)/c;
+        }
+        for(int i=0;i<cores.length;i++){
+            if(time%cores[i]==0){
+                work++;
+                if(work==n){
+                return i+1;
+            }
+            }
+        }
+        return 0;
+    }
+}
