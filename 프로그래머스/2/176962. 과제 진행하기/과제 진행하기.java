23925f4dbf5dc6e96362e7c751e622942b41285c
jiyeongxxxkim <wldud128iop@gmail.com>
2026-08-08 14:42:35 +0900

    [level 2] Title: 과제 진행하기, Time: 8.82 ms, Memory: 91.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
@@ -0,0 +1,49 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[][] plans) {
+        ArrayDeque<Integer> stk = new ArrayDeque<>();
+        String[] planname = new String[plans.length];
+        int[][] plantime = new int[plans.length][3];
+        String[] answer = new String[plans.length];
+        for(int i=0;i<plans.length;i++){
+            planname[i] = plans[i][0];
+            plantime[i][0] = i; 
+            plantime[i][1] = Integer.parseInt(plans[i][1].split(":")[0])*60+Integer.parseInt(plans[i][1].split(":")[1]);
+            plantime[i][2] = Integer.parseInt(plans[i][2]);
+        }
+        Arrays.sort(plantime, (a,b)->{
+           return a[1]-b[1]; 
+        });
+        
+        int resultidx = 0;
+        int curtime = 0;
+
+        for (int i = 0; i < plans.length; i++) {
+            int nextStart = plantime[i][1];
+
+            while (!stk.isEmpty() && curtime < nextStart) {
+                int curIdx = stk.peek();
+                int remainTime = plantime[curIdx][2];
+
+                if (curtime + remainTime <= nextStart) {
+                    curtime += remainTime;
+                    answer[resultidx++] = planname[plantime[stk.pop()][0]];
+                } 
+                else {
+                    plantime[curIdx][2] -= (nextStart - curtime);
+                    curtime = nextStart;
+                    break;
+                }
+            }
+
+            curtime = nextStart;
+            stk.push(i);
+        }
+        while(!stk.isEmpty()){
+            answer[resultidx++] = planname[plantime[stk.pop()][0]];
+        }
+        
+        
+        return answer;
+    }
+}
