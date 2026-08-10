1191f32bb0e75cbe95b1d84d1aa9cd0a3b6b73ab
jiyeongxxxkim <wldud128iop@gmail.com>
2026-08-10 23:41:49 +0900

    [level 2] Title: 점 찍기, Time: 5.20 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -7,7 +7,7 @@ class Solution {
             // 원점과의 거리가 d 이하인 최대 y 좌표 구하기
             // x^2 + y^2 <= d^2  =>  y^2 <= d^2 - x^2  =>  y <= sqrt(d^2 - x^2)
             long maxY = (long) Math.sqrt((long) d * d - x * x);
-            
+             
             // y 좌표 역시 k의 배수만 가능하므로, 0부터 maxY까지 찍을 수 있는 점의 개수
             answer += (maxY / k) + 1;
         }
