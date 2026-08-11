75d8dcd47c7394d134ef88b781f9772d9ad118ec
jiyeongxxxkim <wldud128iop@gmail.com>
2026-08-11 23:34:01 +0900

    [level 2] Title: 점 찍기, Time: 5.33 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -1,17 +1,11 @@
+import java.util.*;
 class Solution {
     public long solution(int k, int d) {
         long answer = 0;
-        
-        // x 좌표를 a * k (0, k, 2k, 3k ...)로 늘려가며 탐색
-        for (long x = 0; x <= d; x += k) {
-            // 원점과의 거리가 d 이하인 최대 y 좌표 구하기
-            // x^2 + y^2 <= d^2  =>  y^2 <= d^2 - x^2  =>  y <= sqrt(d^2 - x^2)
-            long maxY = (long) Math.sqrt((long) d * d - x * x);
-             
-            // y 좌표 역시 k의 배수만 가능하므로, 0부터 maxY까지 찍을 수 있는 점의 개수
-            answer += (maxY / k) + 1;
+        for(int i=0;i<=d;i+=k){
+            answer += (int)(Math.sqrt(Math.pow(d,2)-Math.pow(i,2)))/k+1;
+
         }
-        
         return answer;
     }
 }
