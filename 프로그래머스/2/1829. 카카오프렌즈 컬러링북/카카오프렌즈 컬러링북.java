a520ba3d62365a17929e49ef1731ba3cb5ce0883
jiyeongxxxkim <wldud128iop@gmail.com>
2026-08-30 17:33:31 +0900

    [level 2] Title: 카카오프렌즈 컬러링북, Time: 11.35 ms, Memory: 97.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/1829. 카카오프렌즈 컬러링북/카카오프렌즈 컬러링북.java
@@ -0,0 +1,49 @@
+import java.util.*;
+class Solution {
+    int m, n;
+    int[][] picture;
+    boolean[][] visited;
+    int bfs(int[] start, int val){
+        int[] dr = {1,0,-1,0};
+        int[] dc = {0,1,0,-1};
+        Queue<int[]> q = new LinkedList<>();
+        q.add(start);
+        int count = 0;
+        
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            int cr = cur[0];
+            int cc = cur[1];
+            for(int i=0;i<4;i++){
+                int nr = cr+dr[i];
+                int nc = cc+dc[i];
+                if(nr>=0&&nr<m&&nc>=0&&nc<n){
+                    if(!visited[nr][nc]&&picture[nr][nc]==val){
+                        visited[nr][nc] = true;
+                        count++;
+                        q.add(new int[]{nr, nc});
+                    }
+                }
+            }
+        }
+        return count;
+    }
+    public int[] solution(int m, int n, int[][] picture) {
+        int numberOfArea = 0;
+        int maxSizeOfOneArea = 0;
+        this.m = m;
+        this.n = n;
+        this.picture = picture.clone();
+        visited = new boolean[m][n];
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(picture[i][j]!=0&&!visited[i][j]){
+                    numberOfArea++;
+                    maxSizeOfOneArea = Math.max(maxSizeOfOneArea, bfs(new int[]{i, j}, picture[i][j]));
+                }
+            }
+        }
+        return new int[]{numberOfArea, maxSizeOfOneArea};
+    }
+}
