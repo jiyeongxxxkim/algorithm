fb91a648579ebb7db5764c87fa5b0ee1a29f922d
jiyeongxxxkim <wldud128iop@gmail.com>
2026-09-01 20:28:49 +0900

    [level 2] Title: 점 찍기, Time: 5.39 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -4,7 +4,7 @@ class Solution {
         long answer = 0;
         for(int i=0;i<=d;i+=k){
             answer += (int)(Math.sqrt(Math.pow(d,2)-Math.pow(i,2)))/k+1;
-
+ 
         }
         return answer;
     }
