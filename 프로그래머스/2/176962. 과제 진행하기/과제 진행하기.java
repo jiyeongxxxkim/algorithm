e3711453d707b496b25a034892af708bc94b3416
jiyeongxxxkim <wldud128iop@gmail.com>
2026-09-05 00:27:20 +0900

    [level 2] Title: 과제 진행하기, Time: 6.18 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
@@ -7,7 +7,7 @@ class Solution {
         String[] answer = new String[plans.length];
         for(int i=0;i<plans.length;i++){
             planname[i] = plans[i][0];
-            plantime[i][0] = i; 
+            plantime[i][0] = i;  
             plantime[i][1] = Integer.parseInt(plans[i][1].split(":")[0])*60+Integer.parseInt(plans[i][1].split(":")[1]);
             plantime[i][2] = Integer.parseInt(plans[i][2]);
         }
