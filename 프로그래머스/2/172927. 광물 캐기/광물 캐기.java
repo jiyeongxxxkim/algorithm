d6a1c29803ddb648a756e906c89f4bdc08b4b25c
jiyeongxxxkim <wldud128iop@gmail.com>
2026-09-12 17:05:23 +0900

    [level 2] Title: 광물 캐기, Time: 1.36 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -1,65 +1,42 @@
 import java.util.*;
-
 class Solution {
+    int getpoint(String minr, int gok){
+        if(gok==0)return 1;
+        else if(gok==1){
+            if(minr.equals("diamond"))return 5;
+            else return 1;
+        }else{
+            if(minr.equals("diamond"))return 25;
+            else if(minr.equals("iron"))return 5;
+            else return 1;
+        }
+    }
     public int solution(int[] picks, String[] minerals) {
         int answer = 0;
-        
-        // 1. 가지고 있는 전체 곡괭이 개수 계산
-        int totalPicks = picks[0] + picks[1] + picks[2];
-        
-        // 2. 내가 캘 수 있는 최대 광물의 개수 제한 (중요 ⭐)
-        // 가지고 있는 곡괭이로 다 캘 수 없으면, 어차피 못 캐는 뒤쪽 광물은 잘라내야 정렬이 안 꼬임
-        int maxLen = Math.min(minerals.length, totalPicks * 5);
-        
-        List<int[]> list = new ArrayList<>();
-        
-        // 3. 광물들을 5개씩 묶어서 곡괭이별 피로도 계산
-        for (int i = 0; i < maxLen; i += 5) {
-            int diaFatigue = 0;
-            int ironFatigue = 0;
-            int stoneFatigue = 0;
-            
-            // i부터 최대 5개(또는 남은 광물 끝까지) 검사
-            for (int j = i; j < i + 5 && j < maxLen; j++) {
-                String mineral = minerals[j];
-                
-                if (mineral.equals("diamond")) {
-                    diaFatigue += 1;
-                    ironFatigue += 5;
-                    stoneFatigue += 25;
-                } else if (mineral.equals("iron")) {
-                    diaFatigue += 1;
-                    ironFatigue += 1;
-                    stoneFatigue += 5;
-                } else { // "stone"
-                    diaFatigue += 1;
-                    ironFatigue += 1;
-                    stoneFatigue += 1;
-                }
-            }
-            
-            // 한 묶음의 [다이아 피로도, 철 피로도, 돌 피로도] 배열을 리스트에 추가
-            list.add(new int[]{diaFatigue, ironFatigue, stoneFatigue});
+        int maxlen = 0;
+        for(int p:picks)maxlen+=p;
+        maxlen *= 5;
+        maxlen = Math.min(maxlen, minerals.length);
+        int[][] point = new int[minerals.length/5+1][3];
+        for(int i=0;i<maxlen;i++){
+            point[i/5][0] += getpoint(minerals[i],0);
+            point[i/5][1] += getpoint(minerals[i],1);
+            point[i/5][2] += getpoint(minerals[i],2);
         }
-        
-        // 4. 돌 곡괭이로 캤을 때의 피로도(인덱스 2)를 기준으로 내림차순 정렬
-        Collections.sort(list, (o1, o2) -> {
-            return o2[2] - o1[2]; 
+        Arrays.sort(point, (a,b)->{
+            return b[2]-a[2];
         });
         
-        // 5. 위험도(피로도)가 가장 높은 묶음부터 좋은 곡괭이 할당
-        for (int[] fatigue : list) {
-            if (picks[0] > 0) {         // 다이아 곡괭이가 남았다면
-                answer += fatigue[0];
+        for(int i=0;i<point.length;i++){
+            if(picks[0]!=0){
+                answer += point[i][0];
                 picks[0]--;
-            } else if (picks[1] > 0) {  // 철 곡괭이가 남았다면
-                answer += fatigue[1];
+            }else if(picks[1]!=0){
+                answer += point[i][1];
                 picks[1]--;
-            } else if (picks[2] > 0) {  // 돌 곡괭이가 남았다면
-                answer += fatigue[2];
+            }else if(picks[2]!=0){
+                answer += point[i][2];
                 picks[2]--;
-            } else {
-                break; // 남은 곡괭이가 없으면 종료
             }
         }
         
