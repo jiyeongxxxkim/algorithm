fb4416327555e3c736106a6a95886b037fbb2b7a
jiyeongxxxkim <169635011+jiyeongxxxkim@users.noreply.github.com>
2026-09-18 14:32:18 +0900

    [level 2] Title: 짝지어 제거하기, Time: 30.09 ms, Memory: 67.6 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -3,17 +3,29 @@ class Solution
 {
     public int solution(String s)
     {
+        int answer = 0;
+        if(s.length()%2!=0)return 0;
         Deque<Character> stk = new ArrayDeque<>();
-        char[] arr = s.toCharArray();
-         
-        for(char ch : arr){
-            if(stk.isEmpty()||stk.peek()!=ch){
-                stk.push(ch);
-            }else{
+        int idx = 0;
+        stk.push(s.charAt(idx++));
+        while(idx<s.length()){
+            if(stk.isEmpty()){
+                stk.push(s.charAt(idx++));
+                continue;
+            }
+            if(s.charAt(idx)==stk.peek()){
                 stk.pop();
+                idx++;
+            }else{
+                stk.push(s.charAt(idx++));
             }
         }
-        if(stk.isEmpty())return 1;
-        else return 0;
+        if(stk.size()>1){
+            char c1 = stk.pop();
+            if(c1==stk.peek())return 1;
+            else stk.push(c1);
+        }
+        if(!stk.isEmpty())return 0;
+        else return 1;
     }
 }
