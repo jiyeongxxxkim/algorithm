e51a8172f69459bae09fadeced9922a9d6c991ff
jiyeongxxxkim <169635011+jiyeongxxxkim@users.noreply.github.com>
2026-09-23 23:51:28 +0900

    [level 2] Title: 짝지어 제거하기, Time: 30.70 ms, Memory: 67.2 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -4,7 +4,7 @@ class Solution
     public int solution(String s)
     {
         int answer = 0;
-        if(s.length()%2!=0)return 0;
+        if(s.length()%2!=0)return 0; 
         Deque<Character> stk = new ArrayDeque<>();
         int idx = 0;
         stk.push(s.charAt(idx++));
